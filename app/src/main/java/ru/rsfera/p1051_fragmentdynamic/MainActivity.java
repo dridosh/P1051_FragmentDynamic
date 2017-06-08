75a484b8f1619720529d1b6487a24909c1efef43
Dmitry Ridosh <dridosh@gmail.com>
2017-06-08 16:33:14 +0400

    Расположил элементы в ConstraintLayout

--- app/src/main/java/ru/rsfera/p1051_fragmentdynamic/MainActivity.java
+++ app/src/main/java/ru/rsfera/p1051_fragmentdynamic/MainActivity.java
@@ -3,13 +3,21 @@ package ru.rsfera.p1051_fragmentdynamic;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment1 frag1;
+    Fragment2 frag2;
+    FragmentTransaction fTrans;
+    CheckBox chbStack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        frag1 = new Fragment1();
+        frag2 = new Fragment2();
+
+        chbStack = (CheckBox)findViewById(R.id.chbStack);
+    }
+
+    public void onClick(View v) {
+        fTrans = getSupportFragmentManager().beginTransaction();
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                fTrans.add(R.id.frgmCont, frag1);
+                break;
+            case R.id.btnRemove:
+                fTrans.remove(frag1);
+                break;
+            case R.id.btnReplace:
+                fTrans.replace(R.id.frgmCont, frag2);
+            default:
+                break;
+        }
+        if (chbStack.isChecked()) fTrans.addToBackStack(null);
+        fTrans.commit();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -49,4 +80,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
