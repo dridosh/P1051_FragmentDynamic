7d004775594a8b07023e2f5de666d150d19b6191
Dmitry Ridosh <dridosh@gmail.com>
2017-06-09 17:36:55 +0400

    Повесил листенер на кнопки Сделал проверку перед Add fragment что не он уже не существует Операции Add Replace b remove работают

--- app/src/main/java/ru/rsfera/p1051_fragmentdynamic/MainActivity.java
+++ app/src/main/java/ru/rsfera/p1051_fragmentdynamic/MainActivity.java
@@ -1,22 +1,30 @@
 package ru.rsfera.p1051_fragmentdynamic;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
     Fragment1 frag1;
     Fragment2 frag2;
-    FragmentTransaction fTrans;
+    FragmentTransaction fragmentTransaction;
     CheckBox chbStack;
+
+    FragmentManager fragmentManager;
+    ButtonClickListener buttonClickListener;
+
+    Button btnAdd;
+    Button btnReplace;
+    Button btnRemove;
 
 
     @Override
@@ -25,29 +33,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        fragmentManager = getSupportFragmentManager();
+        buttonClickListener = new ButtonClickListener();
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+        }
 
         frag1 = new Fragment1();
         frag2 = new Fragment2();
 
-        chbStack = (CheckBox)findViewById(R.id.chbStack);
+        //   frag1.setArguments(getIntent().getExtras());
+
+        chbStack = (CheckBox) findViewById(R.id.chbStack);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(buttonClickListener);
+
+        btnReplace = (Button) findViewById(R.id.btnReplace);
+        btnReplace.setOnClickListener(buttonClickListener);
+
+        btnRemove = (Button) findViewById(R.id.btnRemove);
+        btnRemove.setOnClickListener(buttonClickListener);
+
+
     }
 
-    public void onClick(View v) {
-        fTrans = getSupportFragmentManager().beginTransaction();
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                fTrans.add(R.id.frgmCont, frag1);
-                break;
-            case R.id.btnRemove:
-                fTrans.remove(frag1);
-                break;
-            case R.id.btnReplace:
-                fTrans.replace(R.id.frgmCont, frag2);
-            default:
-                break;
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            switch (v.getId()) {
+                case R.id.btnAdd:
+                    Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                    if (!(fragment instanceof Fragment1)) {
+                        fragmentTransaction.add(R.id.fragment_container, frag1);
+                    }
+
+                    break;
+                case R.id.btnRemove:
+                    fragmentTransaction.remove(frag1);
+                    fragmentTransaction.remove(frag2);
+                    break;
+                case R.id.btnReplace:
+                    fragmentTransaction.replace(R.id.fragment_container, frag2);
+                default:
+                    break;
+            }
+            if (chbStack.isChecked()) fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
         }
-        if (chbStack.isChecked()) fTrans.addToBackStack(null);
-        fTrans.commit();
+    }
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -57,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,8 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
 
 
 }
